a43b1eaf55373a8592a99db78a7e02b66c801b84
yura <yuraa0302@gmail.com>
2025-02-27 16:29:21 +0900

    feat : dotenv, typeorm.config.ts 추가

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: process.env.DB_TYPE as any,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  logging: process.env.DB_LOGGING === 'true',
+};
