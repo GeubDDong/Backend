63864dfe247bea248c31d5073680ae2829081221
yura <yuraa0302@gmail.com>
2025-03-09 18:58:33 +0900

    docs :폴더 구조 수정, xlsx 파일 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { ToiletUploadService } from './service/toilet-upload.service';
+import { ToiletUploadController } from './controller/toilet-upload.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig)],
+  providers: [ToiletUploadService],
+  controllers: [ToiletUploadController],
 })
 export class AppModule {}
