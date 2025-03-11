a74f5aa99ae9f972114dbae0b6c78a08a56101ae
yura <yuraa0302@gmail.com>
2025-03-11 20:29:27 +0900

    chore : redis 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { ToiletUploadService } from './service/toilet-upload.service';
 import { ToiletUploadController } from './controller/toilet-upload.controller';
+import { RedisModule } from './configs/redis.module';
+import { RedisService } from './service/redis.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig)],
-  providers: [ToiletUploadService],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), RedisModule],
+  providers: [ToiletUploadService, RedisService],
   controllers: [ToiletUploadController],
 })
 export class AppModule {}
