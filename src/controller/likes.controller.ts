e91c3573be09515cd168f82bfd423ff3aad8fd36
SONGSEUNGSOO <sssong1993@gmail.com>
2025-03-13 18:16:32 +0900

    - commit message : """ feat: 좋아요 기능 구현 issue: [feat] 좋아요 조회, 추가, 삭제 기능 구현 #7 """

--- src/controller/likes.controller.ts
+++ src/controller/likes.controller.ts
@@ -2,39 +2,61 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   ParseIntPipe,
   Post,
+  Req,
 } from '@nestjs/common';
 import { LikesService } from '../service/likes.service';
 import { Public } from 'src/decorators/public.decorator';
+import { Request } from 'express';
 
-@Controller('like')
+@Controller('likes')
 export class LikesController {
   constructor(private readonly likesService: LikesService) {}
 
   // 좋아요 조회 (비로그인)
   @Public()
   @Get(':toiletId/public')
+  @HttpCode(200)
   async getLikesPublic(@Param('toiletId', ParseIntPipe) toiletId: number) {
-    return;
+    return await this.likesService.getLikesPublic(toiletId);
   }
 
-  // 좋아요 조회 (로그인)
+  // // 좋아요 조회 (로그인)
   @Get(':toiletId')
-  async getLikes(@Param('toiletId', ParseIntPipe) toiletId: number) {
-    return;
+  @HttpCode(200)
+  async getLikes(
+    @Param('toiletId', ParseIntPipe) toiletId: number,
+    @Req() req: Request,
+  ) {
+    const { email } = req.user;
+
+    return await this.likesService.getLikes(toiletId, email);
   }
 
   // 좋아요 추가
   @Post(':toiletId')
-  async addLike(@Param('toiletId', ParseIntPipe) toiletId: number) {
-    return;
+  @HttpCode(201)
+  async addLike(
+    @Param('toiletId', ParseIntPipe) toiletId: number,
+    @Req() req: Request,
+  ) {
+    const { email } = req.user;
+
+    return this.likesService.addLike(email, toiletId);
   }
 
   // 좋아요 삭제
   @Delete(':toiletId')
-  async deleteLike(@Param('toiletId', ParseIntPipe) toiletId: number) {
-    return;
+  @HttpCode(200)
+  async deleteLike(
+    @Param('toiletId', ParseIntPipe) toiletId: number,
+    @Req() req: Request,
+  ) {
+    const { email } = req.user;
+
+    return this.likesService.deleteLike(email, toiletId);
   }
 }
--- src/service/likes.service.ts
+++ src/service/likes.service.ts
@@ -1,4 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LikesModel } from 'src/entity/likes.entity';
+import { UsersModel } from 'src/entity/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class LikesService {}
+export class LikesService {
+  constructor(
+    @InjectRepository(LikesModel)
+    private readonly likesRepository: Repository<LikesModel>,
+  ) {}
+
+  //좋아요 조회 (비로그인)
+  async getLikesPublic(toiletId: number) {
+    const likes = await this.likesRepository.count({
+      where: { toilet: { id: toiletId } },
+    });
+
+    return { like: false, count: likes };
+  }
+
+  //좋아요 조회 (로그인)
+  async getLikes(toiletId: number, email: string) {
+    const totalLikes = await this.likesRepository.count({
+      where: { toilet: { id: toiletId } },
+    });
+
+    const user = await this.likesRepository.findOne({
+      where: { user: { email: email }, toilet: { id: toiletId } },
+    });
+
+    if (!user) {
+      return { like: false, count: totalLikes };
+    }
+
+    return { like: true, count: totalLikes };
+  }
+
+  // 좋아요 추가
+  async addLike(email: string, toiletId: number) {
+    const existingLike = await this.likesRepository.findOne({
+      where: { user: { email: email }, toilet: { id: toiletId } },
+    });
+
+    if (existingLike) {
+      throw new ConflictException('이미 좋아요를 추가한 화장실입니다.');
+    }
+
+    const newLike = this.likesRepository.create({
+      user: { email: email },
+      toilet: { id: toiletId },
+    });
+
+    await this.likesRepository.save(newLike);
+
+    const totalLikes = await this.likesRepository.count({
+      where: { toilet: { id: toiletId } },
+    });
+
+    return { like: true, count: totalLikes, message: '좋아요 추가되었습니다.' };
+  }
+
+  // 좋아요 삭제
+  async deleteLike(email: string, toiletId: number) {
+    const existingLike = await this.likesRepository.findOne({
+      where: { user: { email: email }, toilet: { id: toiletId } },
+    });
+
+    if (!existingLike) {
+      throw new ConflictException(
+        '좋아요를 삭제할 화장실이 존재하지 않습니다.',
+      );
+    }
+
+    await this.likesRepository.delete({
+      user: { email: email },
+      toilet: { id: toiletId },
+    });
+
+    const totalLikes = await this.likesRepository.count({
+      where: { toilet: { id: toiletId } },
+    });
+
+    return {
+      like: false,
+      count: totalLikes,
+      message: '좋아요 삭제되었습니다.',
+    };
+  }
+}
