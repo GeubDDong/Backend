e90cc58589a05ffc039e3a6c3736650cca336b70
yura <yuraa0302@gmail.com>
2025-03-14 18:29:41 +0900

    feat : DB에 인천 데이터 추가

--- src/excel/excel.service.ts
+++ src/excel/excel.service.ts
@@ -13,53 +13,49 @@ export class ExcelService {
     private readonly toiletRepository: Repository<ToiletModel>,
   ) {}
 
-  async processExcelFile(filePath: string): Promise<void> {
-    console.log(`📂 엑셀 파일을 읽는 중: ${filePath}`);
+  async processExcelFiles(files: Express.Multer.File[]): Promise<void> {
+    for (const file of files) {
+      const workbook = xlsx.readFile(file.path);
+      const sheetName = workbook.SheetNames[0];
+      const rawData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
-    const workbook = xlsx.readFile(filePath);
-    const sheetName = workbook.SheetNames[0];
-    const rawData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+      const toilets = rawData.map((row: any) => ({
+        name: row['화장실명'] || '알 수 없음',
+        street_address: row['소재지도로명주소'] || '알 수 없음',
+        lot_address: row['소재지지번주소'] || '알 수 없음',
+        disabled_male: row['남성용-장애인용대변기수'] ?? 0,
+        kids_toilet_male: row['남성용-어린이용대변기수'] ?? 0,
+        disabled_female: row['여성용-장애인용대변기수'] ?? 0,
+        kids_toilet_female: row['여성용-어린이용대변기수'] ?? 0,
+        management_agency: row['관리기관명'] || '정보 없음',
+        phone_number: row['전화번호']
+          ? row['전화번호'].toString()
+          : '정보 없음',
+        open_hour:
+          this.convertToStandardOpenHourFormat(row['개방시간상세']) ||
+          '정보 없음',
+        latitude: row['WGS84위도'] ? parseFloat(row['WGS84위도']) : undefined,
+        longitude: row['WGS84경도'] ? parseFloat(row['WGS84경도']) : undefined,
+        emergency_bell: row['비상벨설치여부'] || '정보 없음',
+        cctv: row['화장실입구CCTV설치유무'] || '정보 없음',
+        diaper_changing_station: row['기저귀교환대유무'] || '정보 없음',
+        data_reference_date: row['데이터기준일자'] || '정보 없음',
+      }));
 
-    await this.toiletRepository.query(
-      'TRUNCATE TABLE toilet_model RESTART IDENTITY CASCADE',
-    );
-
-    const toilets = rawData.map((row: any) => ({
-      name: row['화장실명'] || '알 수 없음',
-      street_address: row['소재지도로명주소'] || '알 수 없음',
-      lot_address: row['소재지지번주소'] || '알 수 없음',
-      disabled_male: row['남성용-장애인용대변기수'] ?? 0,
-      kids_toilet_male: row['남성용-어린이용대변기수'] ?? 0,
-      disabled_female: row['여성용-장애인용대변기수'] ?? 0,
-      kids_toilet_female: row['여성용-어린이용대변기수'] ?? 0,
-      management_agency: row['관리기관명'] || '정보 없음',
-      phone_number: row['전화번호'] ? row['전화번호'].toString() : '정보 없음',
-      open_hour:
-        this.convertToStandardOpenHourFormat(row['개방시간상세']) ||
-        '정보 없음',
-      latitude: row['WGS84위도'] ? parseFloat(row['WGS84위도']) : undefined,
-      longitude: row['WGS84경도'] ? parseFloat(row['WGS84경도']) : undefined,
-      emergency_bell: row['비상벨설치여부'] || '정보 없음',
-      cctv: row['화장실입구CCTV설치유무'] || '정보 없음',
-      diaper_changing_station: row['기저귀교환대유무'] || '정보 없음',
-      data_reference_date: row['데이터기준일자'] || '정보 없음',
-    }));
-
-    // 데이터 쪼개서 저장
-    try {
-      const chunkSize = 1000;
-      for (let i = 0; i < toilets.length; i += chunkSize) {
-        const chunk = toilets.slice(i, i + chunkSize);
-        await this.toiletRepository.save(chunk);
+      try {
+        const chunkSize = 1000;
+        for (let i = 0; i < toilets.length; i += chunkSize) {
+          const chunk = toilets.slice(i, i + chunkSize);
+          await this.toiletRepository.save(chunk);
+          console.log(`${i + chunk.length}개 저장 완료`);
+        }
+      } catch (error) {
+        console.log(error);
+        throw error;
       }
-      console.log('저장완료');
-    } catch (error) {
-      console.error('오류:', error);
-      throw error;
-    }
 
-    fs.unlinkSync(filePath);
-    console.log(`파일 삭제 완료: ${filePath}`);
+      fs.unlinkSync(file.path);
+    }
   }
 
   private convertToStandardOpenHourFormat(timeStr: string): string {
@@ -83,10 +79,12 @@ export class ExcelService {
     }
 
     timeStr = timeStr.replace(/-/g, ' ~ ');
+
     const timeRange = timeStr.split('~').map((t) => t.trim());
     if (timeRange.length !== 2) {
       return '정보 없음';
     }
+
     const startTime = moment(timeRange[0], ['HH:mm', 'h:mm A']);
     const endTime = moment(timeRange[1], ['HH:mm', 'h:mm A']);
 
@@ -102,6 +100,7 @@ export class ExcelService {
       .format('A h시')
       .replace('AM', '오전')
       .replace('PM', '오후');
+
     return `${formattedStartTime} ~ ${formattedEndTime}`;
   }
 }
