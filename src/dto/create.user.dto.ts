4914ef539987f3610030cfd8071f65bd8454aa62
yura <yuraa0302@gmail.com>
2025-03-20 17:01:32 +0900

    refactor : toilet.dto swagger 문서화

--- src/dto/create.user.dto.ts
+++ src/dto/create.user.dto.ts
@@ -1,4 +1,5 @@
 export class CreateUserDto {
+  
   email: string;
   id: string;
   username: string;
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -17,6 +18,20 @@ async function bootstrap() {
     origin: 'http://localhost:8080',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('GeubDDong')
+    .setDescription('GeubDDong API Description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addCookieAuth('jwt', {
+      type: 'apiKey',
+      in: 'cookie',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 8080);
 }
