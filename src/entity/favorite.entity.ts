e075dbee7291587d35c22dac331f0fdc9971d1e8
yura <yuraa0302@gmail.com>
2025-04-15 15:57:05 +0900

    fix : favorite entity id를 삭제하고 user_id, toilet_id 복합pk로 설정

--- src/entity/favorite.entity.ts
+++ src/entity/favorite.entity.ts
@@ -1,17 +1,20 @@
 import {
   Entity,
   ManyToOne,
-  PrimaryGeneratedColumn,
   JoinColumn,
   CreateDateColumn,
+  PrimaryColumn,
 } from 'typeorm';
 import { User } from './user.entity';
 import { Toilet } from './toilet.entity';
 
 @Entity('favorites')
 export class Favorite {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn()
+  user_id: number;
+
+  @PrimaryColumn()
+  toilet_id: number;
 
   @ManyToOne(() => User, (user) => user.favorites, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'user_id' })
