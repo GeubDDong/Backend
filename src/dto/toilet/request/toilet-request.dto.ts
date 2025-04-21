ad9424b7970ba4467346044e001c696317294bff
yura <yuraa0302@gmail.com>
2025-04-21 11:59:00 +0900

    fix : query로 들어온 값을 number 값으로 수정

--- src/dto/toilet/request/toilet-request.dto.ts
+++ src/dto/toilet/request/toilet-request.dto.ts
@@ -1,28 +1,35 @@
 import { IsOptional, IsBoolean, IsNumber } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
 export class ToiletRequestDto {
   @ApiProperty()
+  @Type(() => Number)
   @IsNumber()
   cenLat: number;
 
   @ApiProperty()
+  @Type(() => Number)
   @IsNumber()
   cenLng: number;
 
   @ApiProperty()
+  @Type(() => Number)
   @IsNumber()
   top: number;
 
   @ApiProperty()
+  @Type(() => Number)
   @IsNumber()
   bottom: number;
 
   @ApiProperty()
+  @Type(() => Number)
   @IsNumber()
   left: number;
 
   @ApiProperty()
+  @Type(() => Number)
   @IsNumber()
   right: number;
 
