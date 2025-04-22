b02001e81e0474b2e132b4c4df52396539d85ee0
SONGSEUNGSOO <sssong1993@gmail.com>
2025-04-22 12:05:30 +0900

    refactor: extractProfile 반환 타입 명시

--- src/configs/auth/oauth.provider.ts
+++ src/configs/auth/oauth.provider.ts
@@ -28,7 +28,7 @@ export const OAuthProviders: OAuthProviderMap = {
   kakao: {
     tokenUrl: 'https://kauth.kakao.com/oauth/token',
     userInfoUrl: 'https://kapi.kakao.com/v2/user/me',
-    extractProfile: (data) => ({
+    extractProfile: (data): BaseUserProfile => ({
       id: data.id,
       email: data.kakao_account.email,
       profile_image: data.kakao_account.profile?.thumbnail_image_url ?? '',
@@ -38,7 +38,7 @@ export const OAuthProviders: OAuthProviderMap = {
   google: {
     tokenUrl: 'https://oauth2.googleapis.com/token',
     userInfoUrl: 'https://www.googleapis.com/oauth2/v2/userinfo',
-    extractProfile: (data) => ({
+    extractProfile: (data): BaseUserProfile => ({
       id: data.id,
       email: data.email,
       profile_image: data.picture ?? '',
@@ -48,7 +48,7 @@ export const OAuthProviders: OAuthProviderMap = {
   naver: {
     tokenUrl: 'https://nid.naver.com/oauth2.0/token',
     userInfoUrl: 'https://openapi.naver.com/v1/nid/me',
-    extractProfile: (data) => ({
+    extractProfile: (data): BaseUserProfile => ({
       id: data.response.id,
       email: data.response.email,
       profile_image: data.response.profile_image ?? '',
