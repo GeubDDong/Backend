a717c41ef746910e0d33b2229000c532b02081a3
yura <yuraa0302@gmail.com>
2025-04-23 02:51:40 +0900

    feat : 마이페이지 도메인 dto 생성

---
+++ src/dto/mypage/response/mypage-response.dto.ts
@@ -0,0 +1,69 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class MyFavoriteToiletDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  street_address: string;
+
+  @ApiProperty()
+  lot_address: string;
+
+  @ApiProperty()
+  avg_cleanliness: number;
+
+  @ApiProperty()
+  avg_amenities: number;
+
+  @ApiProperty()
+  avg_accessibility: number;
+}
+
+export class ReviewToiletInfoDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+}
+
+export class MyReviewDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  toilet: ReviewToiletInfoDto;
+
+  @ApiProperty()
+  profile_image: string;
+
+  @ApiProperty()
+  nickname: string;
+
+  @ApiProperty()
+  comment: string;
+
+  @ApiProperty()
+  avg_rating: number;
+
+  @ApiProperty()
+  created_at: Date;
+
+  @ApiProperty()
+  updated_at: Date;
+
+  @ApiProperty()
+  isMine: boolean;
+}
+
+export class MyPageResponseDto {
+  @ApiProperty({ type: [MyFavoriteToiletDto] })
+  favorites: MyFavoriteToiletDto[];
+
+  @ApiProperty({ type: [MyReviewDto] })
+  reviews: MyReviewDto[];
+}
