226fe6d193635c5867800aa93bac92df953c262a
SONGSEUNGSOO <sssong1993@gmail.com>
2025-04-22 12:05:10 +0900

    refactor: samSite 설정

--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -24,11 +24,13 @@ export class OauthController {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: true,
+      sameSite: 'none',
     });
 
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: true,
+      sameSite: 'none',
     });
 
     return {
