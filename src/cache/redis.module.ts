2d6dec6b0a841b41335d7f29bc9621e1d69f9e23
yura <yuraa0302@gmail.com>
2025-04-28 16:29:45 +0900

    !hotfix : 급한 버그 수정

--- src/cache/redis.module.ts
+++ src/cache/redis.module.ts
@@ -1,30 +1,30 @@
-// import { Global, Module } from '@nestjs/common';
-// import Redis from 'ioredis';
-// import * as dotenv from 'dotenv';
-// import { RedisService } from './redis.service';
+import { Global, Module } from '@nestjs/common';
+import Redis from 'ioredis';
+import * as dotenv from 'dotenv';
+import { RedisService } from './redis.service';
 
-// dotenv.config();
+dotenv.config();
 
-// @Global()
-// @Module({
-//   providers: [
-//     {
-//       provide: 'REDIS_CLIENT',
-//       useFactory: () => {
-//         const client = new Redis({
-//           host: process.env.REDIS_HOST,
-//           port: Number(process.env.REDIS_PORT),
-//           password: process.env.REDIS_PASSWORD,
-//         });
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'REDIS_CLIENT',
+      useFactory: () => {
+        const client = new Redis({
+          host: process.env.REDIS_HOST,
+          port: Number(process.env.REDIS_PORT),
+          password: process.env.REDIS_PASSWORD,
+        });
 
-//         client.on('connect', () => console.log('Redis connected'));
-//         client.on('error', (err) => console.log(`Redis error: ${err}`));
+        client.on('connect', () => console.log('Redis connected'));
+        client.on('error', (err) => console.log(`Redis error: ${err}`));
 
-//         return client;
-//       },
-//     },
-//     RedisService,
-//   ],
-//   exports: ['REDIS_CLIENT', RedisService],
-// })
-// export class RedisModule {}
+        return client;
+      },
+    },
+    RedisService,
+  ],
+  exports: ['REDIS_CLIENT', RedisService],
+})
+export class RedisModule {}
--- src/cache/redis.service.ts
+++ src/cache/redis.service.ts
@@ -1,54 +1,54 @@
-// import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common';
-// import { rejects } from 'assert';
-// import Redis from 'ioredis';
-// import { resolve } from 'path';
+import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common';
+import { rejects } from 'assert';
+import Redis from 'ioredis';
+import { resolve } from 'path';
 
-// @Injectable()
-// export class RedisService implements OnModuleDestroy {
-//   constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
+@Injectable()
+export class RedisService implements OnModuleDestroy {
+  constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
 
-//   async set(key: string, value: any, ttl?: number): Promise<void> {
-//     const stringValue = JSON.stringify(value);
-//     if (ttl) {
-//       await this.redisClient.set(key, stringValue, 'EX', ttl); // TTL 적용
-//     } else {
-//       await this.redisClient.set(key, stringValue);
-//     }
-//   }
+  async set(key: string, value: any, ttl?: number): Promise<void> {
+    const stringValue = JSON.stringify(value);
+    if (ttl) {
+      await this.redisClient.set(key, stringValue, 'EX', ttl); // TTL 적용
+    } else {
+      await this.redisClient.set(key, stringValue);
+    }
+  }
 
-//   async get<T>(key: string): Promise<T | null> {
-//     const value = await this.redisClient.get(key);
-//     return value ? JSON.parse(value) : null;
-//   }
+  async get<T>(key: string): Promise<T | null> {
+    const value = await this.redisClient.get(key);
+    return value ? JSON.parse(value) : null;
+  }
 
-//   async del(key: string): Promise<number> {
-//     return this.redisClient.del(key);
-//   }
+  async del(key: string): Promise<number> {
+    return this.redisClient.del(key);
+  }
 
-//   async flushAll(): Promise<void> {
-//     await this.redisClient.flushall();
-//   }
+  async flushAll(): Promise<void> {
+    await this.redisClient.flushall();
+  }
 
-//   async scanKeys(pattern: string): Promise<string[]> {
-//     const stream = this.redisClient.scanStream({ match: pattern });
-//     const keys: string[] = [];
+  async scanKeys(pattern: string): Promise<string[]> {
+    const stream = this.redisClient.scanStream({ match: pattern });
+    const keys: string[] = [];
 
-//     return new Promise((resolve, reject) => {
-//       stream.on('data', (resultKeys: string[]) => {
-//         for (const key of resultKeys) {
-//           keys.push(key);
-//         }
-//       });
-//       stream.on('end', () => {
-//         resolve(keys);
-//       });
-//       stream.on('err', (err) => {
-//         reject(err);
-//       });
-//     });
-//   }
+    return new Promise((resolve, reject) => {
+      stream.on('data', (resultKeys: string[]) => {
+        for (const key of resultKeys) {
+          keys.push(key);
+        }
+      });
+      stream.on('end', () => {
+        resolve(keys);
+      });
+      stream.on('err', (err) => {
+        reject(err);
+      });
+    });
+  }
 
-//   onModuleDestroy() {
-//     this.redisClient.quit();
-//   }
-// }
+  onModuleDestroy() {
+    this.redisClient.quit();
+  }
+}
