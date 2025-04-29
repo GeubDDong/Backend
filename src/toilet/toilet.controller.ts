c732924b2ff05c141c9fce4246f93788c57f3242
yura <yuraa0302@gmail.com>
2025-04-29 12:38:56 +0900

    !hotfix : toilet 도메인 회원/비회원 컨트롤러 수정

--- src/toilet/toilet.controller.ts
+++ src/toilet/toilet.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Query, Req } from '@nestjs/common';
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
 import { Public } from 'src/decorator/public.decorator';
 import { ToiletService } from './toilet.service';
 import { Request } from 'express';
 import { ToiletMapRequestPipe } from './pipes/toilet-map-transform.pipe';
+import { OptionalJwtAuthGuard } from 'src/util/guards/optional-jwt-auth.guard';
 
 @Controller('toilet')
+@UseGuards(OptionalJwtAuthGuard)
 export class ToiletController {
   constructor(private readonly toiletService: ToiletService) {}
 
